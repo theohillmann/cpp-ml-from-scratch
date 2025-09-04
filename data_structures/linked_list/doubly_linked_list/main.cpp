bdc27466dc87b5f03fea16b6181ebce5ca584da3
Theo <theohillmann@hotmail.com>
2025-09-04 14:51:20 -0300

    feat: Add insert_back method to DoublyLinkedList for appending elements

--- data_structures/linked_list/doubly_linked_list/main.cpp
+++ data_structures/linked_list/doubly_linked_list/main.cpp
@@ -21,6 +21,16 @@ public:
         }
     }
 
+    void insert_back(int value){
+        if (!head){
+            head = tail = new Node(value, nullptr, nullptr);
+        }else{
+            Node* temp = tail;
+            tail = new Node(value, nullptr, temp);
+            temp->next = tail;
+        }
+    }
+
     void display_foward() {
         Node* temp = head;
         while (temp){
@@ -47,6 +57,7 @@ int main() {
 
     list.insert_front(10);
     list.insert_front(20);
+    list.insert_back(30);
     list.display_foward();
     list.display_backward();
 
